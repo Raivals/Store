038be916dd33b76f8ce58d49c962845beb37f68a
Raivals <103427644+Raivals@users.noreply.github.com>
2022-11-29 12:07:03 +0100

    Ajout Darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 // import stuff routes & componentss here
+import { createContext, useState } from "react"
+import React from "react"
 import { Routes, Route } from "react-router-dom"
 import { Container } from "react-bootstrap"
 import Home from "./pages/Home"
@@ -6,19 +8,41 @@ import Store from "./pages/Store"
 import About from "./pages/About"
 import Navbar from "./components/Navbar"
 import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+import "./App.css"
+import ReactSwitch from "react-switch"
+
+export const ThemeContext = React.createContext(null)
 
 function App() {
+  const [theme, setTheme] = useState("light")
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"))
+  }
+
   return (
-    <ShoppingCartProvider>
-      <Navbar />
-      <Container className="mb-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Container>
-    </ShoppingCartProvider>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div id={theme}>
+        <ShoppingCartProvider>
+          <Navbar />
+          <ReactSwitch
+            className="d-flex align-items-center"
+            style={{
+              visibility: "hidden",
+            }}
+            onChange={toggleTheme}
+            checked={theme === "dark"}
+          />
+          <Container className="mb-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/store" element={<Store />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </Container>
+        </ShoppingCartProvider>
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
